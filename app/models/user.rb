35b82b0a029b86d26853bc9ef1afeb540925aeeb
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-04-01 09:25:34 +0000

    [Add]いいね多い順実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
+  
   # フォローの関連付け
   has_many :active_relationships, class_name: "Relationship",  foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship",  foreign_key: "followed_id", dependent: :destroy
