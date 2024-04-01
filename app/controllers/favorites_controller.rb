4704b818f86a21e952df7c8610d1533e3181aab0
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-04-01 07:50:23 +0000

    [Add]非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,6 @@
 class FavoritesController < ApplicationController
   def create
+    @book = Book.find(params[:book_id])
     @favorite = current_user.favorites.create(book_id: params[:book_id])
     # redirect_to request.referer
   end
